424f2d2f6e21e1bb37e3078f61b7af006d7a3f4d
Megan Sullivan <meganesulli@gmail.com>
2018-02-13 15:35:14 -0500

    Create activity list landing page

--- activities/views.py
+++ activities/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Activity
 
 # Create your views here.
+def activity_list(request):
+  activities = Activity.objects.order_by('published_date')
+  return render(request, 'activities/activity_list.html', {})
